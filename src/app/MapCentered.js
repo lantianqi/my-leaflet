05df715a6ba6c390e2346bc77c97255c4e601b3f
lantianqi <50139994+lantianqi@users.noreply.github.com>
2024-05-13 21:03:00 +1000

    Map component takes props lon&lat for center

--- src/app/MapCentered.js
+++ src/app/MapCentered.js
@@ -2,14 +2,17 @@ import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet';
 import "./page.css";
 import "leaflet/dist/leaflet.css";
 
-export default function MyMap() {
+export default function MyMap({
+  longitude,
+  latitude
+}) {
   return (
-    <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false} style={{ flex: 1, height: '100%', width: '100%' }}>
+    <MapContainer center={[longitude, latitude]} zoom={1} scrollWheelZoom={false} style={{ flex: 1, height: '100%', width: '100%' }}>
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={[51.505, -0.09]}>
+      <Marker position={[longitude, latitude]}>
         <Popup>
           A pretty CSS3 popup. <br /> Easily customizable.
         </Popup>
